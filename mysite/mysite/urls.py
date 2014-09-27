5419ef70998cd51e3498d2c76f5d45cb54a19eac
LenaTech <lena.wie.gehts@gmail.com>
2014-09-27 14:43:21 +0800

    Add view Function Hello World

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    # http://localhost/hello/
+    url(r'^hello/$', 'trips.views.hello_world'),
 )
