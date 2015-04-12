a0518932c62f908eb55160af61d17445c316ec30
lenatech <lena.wie.gehts@gmail.com>
2015-04-12 16:06:02 +0800

    adjust the tabs to spaces

--- mysite/accounts/models.py
+++ mysite/accounts/models.py
@@ -3,12 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class UserProfile(models.Model):
-		# This line is required. Links UserProfile to a User model instance.
-		user = models.OneToOneField(User)
-		# The additional attributes we wish to include.
-		website = models.URLField(blank=True)
-		avatar = models.ImageField(upload_to='profile_images', blank=True)
+        # This line is required. Links UserProfile to a User model instance.
+        user = models.OneToOneField(User)
+        # The additional attributes we wish to include.
+        website = models.URLField(blank=True)
+        avatar = models.ImageField(
+            upload_to='images/avatar',
+            default = 'images/avatar/default.jpg', 
+            blank=True)
 
-		# Override the __unicode__() method to return out something meaningful!
-		def __unicode__(self):
-			return self.user.username
+        # Override the __unicode__() method to return out something meaningful!
+        def __unicode__(self):
+            return self.user.username
